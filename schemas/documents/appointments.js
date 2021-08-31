36cecff1e7b523ae9b9331a4c87d390b550f8a59
Hagen Strahl <hagen.strahl@gmail.com>
2021-08-31 12:32:51 +0200

    first steps

---
+++ schemas/documents/appointments.js
@@ -0,0 +1,12 @@
+export default {
+  type: "document",
+  name: "appointment",
+  title: "Veranstaltungen",
+  fields: [
+    {
+      type: "string",
+      name: "title",
+      title: "Titel",
+    },
+  ],
+};
